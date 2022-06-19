2cf7d6fa5a4fdb3750e2a9d7835246b13c8fc057
Steve <stevestranczek@gmail.com>
2022-06-19 15:31:14 -0600

    pick best food job working

--- index.ts
+++ index.ts
@@ -37,21 +37,27 @@ let deleteFoodMessages = new CronJob("0 23 * * WED", () => {
   );
 });
 
-let PickBestFood = new CronJob("0 15 * * WED", () => {
+let pickBestFood = new CronJob("0 15 * * WED", () => {
   (client.channels.cache.get("988189553661722626") as TextChannel).messages
     .fetch({ limit: 50 })
     .then((messages) => {
+      let tuple: [number, string] = [0, "test"];
       messages.forEach((message) => {
         let count = message.reactions.cache.get("👍")?.count;
-        if (count != null && count > 1) {
-          (client.channels.cache.get("982622537395609642") as TextChannel).send(
-            message.content + " is what we will be eating!"
-          );
+        if (count != null && count > tuple[0]) {
+          tuple[0] = count;
+          tuple[1] = message.content;
         }
       });
+      (client.channels.cache.get("859497250732244995") as TextChannel).send(
+        "@everyone " +
+          tuple[1] +
+          " is what we will be eating!  Please send a message saying what you want!"
+      );
     });
 });
 
 deleteFoodMessages.start();
+pickBestFood.start();
 
 client.login(process.env.TOKEN);
